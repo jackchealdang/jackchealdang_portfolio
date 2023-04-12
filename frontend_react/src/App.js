b860a71127182585b9aa4cb5866caf2056be8cff
jackchealdang <jackchealdang@gmail.com>
2023-04-12 08:10:14 -0500

    hide about

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <div className="app">
       <Navbar />
       <Header />
-      <About />
+      {/* <About /> */}
       {/* <Work /> */}
       <Skills />
       {/* <Testimonial /> */}
